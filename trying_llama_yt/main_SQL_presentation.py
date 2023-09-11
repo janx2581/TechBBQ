bf8b3a655036760b7b15443263f9c3df5759499e
janx2581 <143491736+janx2581@users.noreply.github.com>
2023-09-11 10:26:19 +0200

    changed to google database

--- trying_llama_yt/main_SQL_presentation.py
+++ trying_llama_yt/main_SQL_presentation.py
@@ -3,16 +3,45 @@ import sqlite3
 import pandas as pd
 from streamlit_autorefresh import st_autorefresh
 import time
+from google.oauth2 import service_account
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Secrets work
+service_account_json = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+  "universe_domain": st.secrets["universe_domain"]
+}
+
+#Scope
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
+
+# Credentials
+credentials = Credentials.from_service_account_info(service_account_json, scopes=scope)
+client = gspread.authorize(credentials)
 
 st.markdown("""
     <style>
         table {width:100%;}
-        th {font-size:1em;}
+        th {font-size:0.8em;}
         td {font-size:1em; white-space: pre-wrap; word-wrap: break-word;}
     </style>
 """, unsafe_allow_html=True)
 
-st.title('Submission Viewer')
+st.title('Submission Viewer for HTHC AI 🔴⚪️')
+st.markdown('<h2 style="font-size:1.5em;">Create something creative about healthtech! Scan the QR code below</h2>', unsafe_allow_html=True)
+st.markdown('<h2 style="font-size:1em;">(I need to make this a bit more pretty)</h2>', unsafe_allow_html=True)
+st.image('/Users/janhoegh/Documents/GitHub/TechBBQ/trying_llama_yt/icon white.png', width=25)
+
 
 countdown_time = 5  # Adjust the countdown time as per your requirements
 countdown_timer = st.empty()
@@ -21,38 +50,45 @@ page_number = st.empty()
 table = st.empty()
 
 def get_total_entries():
-    conn = sqlite3.connect('submissions.db')
-    total_entries = pd.read_sql_query("SELECT COUNT(*) FROM submissions", conn).iloc[0, 0]
-    conn.close()
+    spreadsheet = client.open('HTHC-techbbq-sheet')
+    worksheet = spreadsheet.get_worksheet(0)  # Assuming data is in the first worksheet
+    total_entries = len(worksheet.get_all_values()) - 1  # Subtract 1 to exclude header row
     return total_entries
-
-def load_data(offset, limit):
-    conn = sqlite3.connect('submissions.db')
-    query = f"SELECT * FROM submissions LIMIT {limit} OFFSET {offset}"
-    data = pd.read_sql_query(query, conn)
-    data = data.drop(columns=['id', 'name', 'email'])  # Dropping the specified columns
-    conn.close()
-    return data
 
 total_entries = get_total_entries()
 current_page = 0
 
-PAGE_SIZE = 5  # Set this to the number of rows you want to display on each page
+PAGE_SIZE = 3  # Set this to the number of rows you want to display on each page
+
+
+def load_data(offset, limit):
+    spreadsheet = client.open('HTHC-techbbq-sheet')
+    worksheet = spreadsheet.get_worksheet(0)  # Assuming data is in the first worksheet
+    data = worksheet.get_all_values()
+    if data:
+        df = pd.DataFrame(data[1:], columns=data[0])  # Convert data to pandas DataFrame
+        df.drop(df.columns[[0, 1]], axis=1, inplace=True)  # Drop the first two columns
+    else:
+        df = pd.DataFrame()
+    return df
+
 
 while True:
     offset = current_page * PAGE_SIZE
     data = load_data(offset, PAGE_SIZE)
 
-    if data.empty and current_page != 0:
+    display_data = data[offset:offset + PAGE_SIZE]
+
+    if display_data.empty and current_page != 0:
         current_page = 0
     else:
-        page_number.markdown(f"### Page: {current_page + 1}/{(total_entries // PAGE_SIZE) + 1}")
-        table.table(data)
+        page_number.markdown(
+            f'<p style="font-size:0.8em;">Page: {current_page + 1}/{(total_entries // PAGE_SIZE) + 1}</p>',
+            unsafe_allow_html=True)
+        table.table(display_data)
         current_page += 1
 
-    # time.sleep(5)
     for i in range(countdown_time, 0, -1):
         countdown_timer.write(f"Next slide in: {i} seconds", unsafe_allow_html=True)
         time.sleep(1)
         countdown_timer.empty()
-

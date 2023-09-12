82fe6cb5863f46cc96dbf601224746e3d03fb541
janx2581 <143491736+janx2581@users.noreply.github.com>
2023-09-12 11:17:47 +0200

    Update main_SQL_presentation.py

--- trying_llama_yt/main_SQL_presentation.py
+++ trying_llama_yt/main_SQL_presentation.py
@@ -116,8 +116,10 @@ while True:
             countdown_timer.empty()
 
             # Display the new entry prominently
+            st.markdown('<h2 style="font-size:1em;"><i>New Entry:</i></h2>',
+                        unsafe_allow_html=True)
             new_entry_display.markdown(
-                f"<div style='font-size:3em;'><i>New Entry:</i> <br><p style='font-size:1em;'>{new_entry_text}</p></div>",
+                f"<div style='font-size:3em;'>{new_entry_text}</div>",
                 unsafe_allow_html=True)
 
         # Clear the new entry display

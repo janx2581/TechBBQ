5e6f9c51802706d5517d544b5e72ae21ecc9f908
janx2581 <143491736+janx2581@users.noreply.github.com>
2023-09-12 11:03:16 +0200

    Update main_SQL_presentation.py

--- trying_llama_yt/main_SQL_presentation.py
+++ trying_llama_yt/main_SQL_presentation.py
@@ -49,6 +49,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
+st.set_page_config(page_title="User submissions", layout="wide")
 st.title('🔴⚪ User submissions for HTHC AI competition! 🔴⚪️')
 st.markdown('<h2 style="font-size:1.5em;">Create something creative about healthtech! Scan the QR code below</h2>', unsafe_allow_html=True)
 st.markdown('<h2 style="font-size:1em;">(I need to make this a bit more pretty)</h2>', unsafe_allow_html=True)

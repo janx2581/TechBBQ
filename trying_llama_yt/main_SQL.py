2f35a7967f545eeb5d50fc30e4fc2de1a8fd8e00
janx2581 <143491736+janx2581@users.noreply.github.com>
2023-09-12 15:34:52 +0200

    Update main_SQL.py

--- trying_llama_yt/main_SQL.py
+++ trying_llama_yt/main_SQL.py
@@ -185,23 +185,27 @@ if st.session_state['show_form']:
         participant_name = st.text_input("Name", value=st.session_state.get('participant_name', ''))
         participant_email = st.text_input("Email", value=st.session_state.get('participant_email', ''))
         participant_entry = st.text_area("Your Creative Text", value=st.session_state.get('latest_response', ''))
-        consent = st.checkbox("I consent to the sharing of my personal data.")
-
+        consent = st.checkbox("I consent to the sharing of my personal data with HTHC.")
         if consent:
-            if submit_button = st.form_submit_button(label='Submit Entry'):
-                data_dict = {'Name': participant_name, 'Email': participant_email, 'Creative Text': participant_entry}
-
-                # Add a new row to the Google Sheets document
-                worksheet.append_row([data_dict['Name'], data_dict['Email'], data_dict['Creative Text']])
-
-                st.success('Submitted in google, thank you!')
-
-                clear_form()
-
-                for i in range(5, 0, -1):
-                    st.write(f"New session in: {i}")
-                    time.sleep(1)
-
-                st.experimental_rerun()
+            submit_button = st.form_submit_button(label='Submit Entry')
         else:
             st.write("Please consent to the sharing of personal data to submit the form.")
+        submit_button = st.form_submit_button(label='Submit Entry')
+
+if submit_button:
+    data_dict = {'Name': participant_name, 'Email': participant_email, 'Creative Text': participant_entry}
+
+    # Add a new row to the Google Sheets document
+    worksheet.append_row([data_dict['Name'], data_dict['Email'], data_dict['Creative Text']])
+
+    st.success('Submitted in google, thank you!')
+
+    clear_form()
+
+    for i in range(5, 0, -1):
+        st.write(f"New session in: {i}")
+        time.sleep(1)
+
+    st.experimental_rerun()
+
+

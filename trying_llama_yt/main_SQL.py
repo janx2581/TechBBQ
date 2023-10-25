723e468185b66bc409acfd64d22fc71a6b86f513
janx2581 <143491736+janx2581@users.noreply.github.com>
2023-10-25 18:24:21 +0200

    Update main_SQL.py

--- trying_llama_yt/main_SQL.py
+++ trying_llama_yt/main_SQL.py
@@ -48,7 +48,7 @@ st.set_page_config(page_title="HTHC AI 🔴⚪️", initial_sidebar_state="expan
 # Set sidebar
 with st.sidebar:
     st.title('Welcome to the HTHC AI chatbot️')
-    st.markdown('A demo for Niels Storgaard / Policy Group')
+    st.markdown('A demo for Schultz Jørgensen Kom')
     st.markdown('📖 Learn more about Health Tech Hub Copenhagen [here](https://healthtechhub.org/)!')
     st.markdown('*Read about Health Tech Hub Copenhagens privacy policy [here](https://healthtechhub.org/privacy-policy/)*')
 
@@ -168,7 +168,7 @@ def generate_llama2_response(prompt_input):
         # 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # The 13B parameter model
         "meta/llama-2-70b-chat:35042c9a33ac8fd5e29e27fb3197f33aa483f72c2ce3b0b9d201155c7fd2a287", # The 70B parameter model
         input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-               "temperature": 0.1, "top_p": 0.9, "max_length": 200000000000000000, "repetition_penalty": 1})
+               "temperature": 0.1, "top_p": 0.9, "max_length": 2000, "repetition_penalty": 1})
     # Get the output as a string
     output = ''.join(output)
 
